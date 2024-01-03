58bd94e993e3e1ac7d8c7fa90231e9cf44c0e525
Michael Pautov <michael.pautov@gmail.com>
2024-01-03 14:01:55 +0600

    feat(app): added html issue

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Web() {
   return (
     <>
       <span>
-        <div>Wrong place for text 3</div>
+        <div>Wrong place for text 4</div>
       </span>
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
