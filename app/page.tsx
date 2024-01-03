10c7650a8dd5fa445ded287ec078ea0c57e2cb08
Michael Pautov <michael.pautov@gmail.com>
2024-01-03 14:06:07 +0600

    feat(app): added html issue

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export const metadata: Metadata = {
 export default function Web() {
   return (
     <>
+      <p>test</p>
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
