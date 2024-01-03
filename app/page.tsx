97172086a9189ed797d502adc43d0a265b4c7878
Michael Pautov <michael.pautov@gmail.com>
2024-01-03 13:31:23 +0600

    feat(app): added html issue

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 export default function Web() {
   return (
     <>
-      <span><div>Wrong place for text<div/></span>
+      <span><div>Wrong place for text</div></span>
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
