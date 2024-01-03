67e99e7df3b2d59e94951e05152d302c144cad57
Michael Pautov <michael.pautov@gmail.com>
2024-01-03 14:16:39 +0600

    feat(app): added html issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import "styles/tailwind.css"
 
+
+function testFunction() {
+  let a = '1233'
+  return a + '321'
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  console.log(testFunction());
   return (
     <html lang="en">
       <body>{children}</body>
